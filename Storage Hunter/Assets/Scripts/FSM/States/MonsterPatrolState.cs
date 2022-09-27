0c494d7fe59a6b37dd7285db71b98e911158f1aa
goatopia <85709994+goatopia@users.noreply.github.com>
2022-09-26 20:58:45 -0400

    patrol script update
    it may be functional i need to test it

--- Storage Hunter/Assets/Scripts/FSM/States/MonsterPatrolState.cs
+++ Storage Hunter/Assets/Scripts/FSM/States/MonsterPatrolState.cs
@@ -7,28 +7,50 @@ using UnityEngine;
 public class MonsterPatrolState : MonsterBaseState
 {
     GameObject monster;
+    GameObject monsterGameObject;
     GameObject[] waypoints; //waypoints for the monster to patrol between
-    Transform[] wpTransforms; //we need the transforms of each waypoint
+    //Transform[] wpTransforms; //we need the transforms of each waypoint
 
+    private float minDistance;
+    private float moveSpeed = 5f;
     public Transform currentWaypoint;
     public int currentIndex;
+
 
     public override void EnterState(MonsterStateManager monster)
     {
         Debug.Log("Monster is now in the patrol state");
+        monsterGameObject = GameObject.FindGameObjectWithTag("monster");
         waypoints = GameObject.FindGameObjectsWithTag("waypoint");
-        wpTransforms = new Transform[waypoints.Length];
+        //wpTransforms = new Transform[waypoints.Length];
 
+        /*
         for(int i=0; i < waypoints.Length; i++)
         {
             //get the transform values of each waypoint in our array
             wpTransforms[i] = waypoints[i].transform;
         }
+        */
+
+        //currentWaypoint = wpTransforms[0];
+        currentWaypoint = waypoints[0].transform;
     }
 
     public override void UpdateState(MonsterStateManager monster)
     {
+        Vector3 direction = currentWaypoint.transform.position - monsterGameObject.transform.position;
+        Vector3 moveVector = direction.normalized * moveSpeed * Time.deltaTime;
+        monsterGameObject.transform.position += moveVector;
 
+        if(Vector3.Distance(currentWaypoint.transform.position, monsterGameObject.transform.position) < minDistance)
+        {
+            ++currentIndex;
+            if(currentIndex > waypoints.Length - 1)
+            {
+                currentIndex = 0;
+            }
+            currentWaypoint = waypoints[currentIndex].transform;
+        }
     }
 
     public override void OnCollisionEnter(MonsterStateManager monster)
