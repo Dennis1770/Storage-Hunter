45647395b5203bd1f7a329b065287375127f6771
LeeAaronVan <114447600+LeeAaronVan@users.noreply.github.com>
2022-10-04 17:23:29 -0400

    Improved Menu Settings

---
+++ Storage Hunter/Assets/Scripts/MenuController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+
+
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
+
+
+    [Header("Controls Settings")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4;
+    public int mainControllerSen = 4;
+
+
+    [Header("Gameplay Settings")]
+    private int qualityLevel;
+    private bool _isFullscreen;
+
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullscreenToggle;
+
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
+    [Header("Resolution Dropdowns")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        _isFullscreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        qualityLevel = qualityIndex;
+    }
+
+    public void GameplayApply()
+    {
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullscreen ? 1 : 0));
+        Screen.fullScreen = _isFullscreen;
+
+        StartCoroutine(ConfirmationBox());
+    }
+
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Gameplay")
+        {
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullscreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GameplayApply();
+
+
+        }
+
+    }
+
+
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+
+
+}
