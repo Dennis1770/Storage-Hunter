23566590951a846a7a8df948fba9ebea5301bc85
LeeAaronVan <114447600+LeeAaronVan@users.noreply.github.com>
2022-10-25 19:39:02 -0400

    Raycast objects and canvas prompt

--- Storage Hunter/Assets/Scripts/FPS Controller/playerRaycast.cs
+++ Storage Hunter/Assets/Scripts/FPS Controller/playerRaycast.cs
@@ -21,7 +21,32 @@ public class playerRaycast : MonoBehaviour
     private RaycastHit hit;
 
 
-    // Update is called once per frame
+    public static bool pickableObject1Retrieved;
+    public static bool pickableObject2Retrieved;
+
+    [Header("Pickable Objects")]
+    [SerializeField]
+    private GameObject pickableObject1;
+    [SerializeField]
+    private GameObject pickableObject2;
+
+    [Header("Pickup Audio Clip")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private void Awake()
+    {
+        if (pickableObject1Retrieved == true)
+        {
+            Destroy(pickableObject1);
+        }
+
+        if (pickableObject2Retrieved == true)
+        {
+            Destroy(pickableObject2);
+        }
+
+    }
     private void Update()
     {
         Debug.DrawRay(playerCameraTransform.position, playerCameraTransform.forward * hitRange, Color.red);
@@ -39,8 +64,51 @@ public class playerRaycast : MonoBehaviour
         {
             hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
             pickUpUI.SetActive(true);
-        }
 
+
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log(hit.transform.name);
+
+                if (hit.transform.name == "PickableObject1")
+                {
+                    // Play pickup sfx
+                    audioSource.PlayOneShot(audioClip);
+                    Debug.Log("Playing pickup sfx");
+
+                    Debug.Log("SetActive false PickableObject1");
+                    Destroy(hit.transform.gameObject);
+
+
+                    // setting bool to true after picking up object
+                    pickableObject1Retrieved = true;
+
+
+                    // when destroyed set UI to false
+                    pickUpUI.SetActive(false);
+                }
+
+                if (hit.transform.name == "PickableObject2")
+                {
+                    // Play pickup sfx
+                    audioSource.PlayOneShot(audioClip);
+                    Debug.Log("Playing pickup sfx");
+
+                    Debug.Log("SetActive false PickableObject2");
+                    Destroy(hit.transform.gameObject);
+
+
+                    // setting bool to true after picking up object
+                    pickableObject2Retrieved = true;
+
+
+                    // when destroyed set UI to false
+                    pickUpUI.SetActive(false);
+                }
+
+            }
+        }
 
 
 
