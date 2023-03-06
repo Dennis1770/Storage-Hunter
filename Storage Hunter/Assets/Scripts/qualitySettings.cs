274f02fda430652eb4a901807276919b74f2b453
megatronus199 <megatronus99@gmail.com>
2023-03-06 17:40:00 -0500

    Fixed Quality Settings
    Now changes will apply when selected, though it takes a couple seconds

--- Storage Hunter/Assets/Scripts/qualitySettings.cs
+++ Storage Hunter/Assets/Scripts/qualitySettings.cs
@@ -1,28 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.Rendering;
 
 public class qualitySettings : MonoBehaviour
 {
-
-    public RenderPipelineAsset[] qualityLevels; //array for URP render pipeline settings
-    public TMP_Dropdown dropdown; //text mesh pro dropdown menu
     
-    // Start is called before the first frame update
-    void Start()
+    public void SetQuality(int qualityIndex)
     {
 
-        dropdown.value = QualitySettings.GetQualityLevel(); //default quality level
+        QualitySettings.SetQualityLevel(qualityIndex, false); //chooses from project settings
+        Debug.Log("Changed Settings");
 
-    }
-
-    public void ChnageLevel(int value)
-    {
-
-        QualitySettings.SetQualityLevel(value); //set quality to selected option
-        QualitySettings.renderPipeline = qualityLevels[value]; //select different qualuty from list
     }
 
 }
