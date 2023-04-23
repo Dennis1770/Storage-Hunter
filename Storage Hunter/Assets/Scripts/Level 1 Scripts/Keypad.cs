8abd59b7b212418aeb930af737f1142da8d02a79
LeeAaronVan <lee.aaron.van@gmail.com>
2023-04-22 23:52:59 -0400

    Level 1 Light intensity increased, Level 2 Lights added, Level 3 Layout

--- Storage Hunter/Assets/Scripts/Level 1 Scripts/Keypad.cs
+++ Storage Hunter/Assets/Scripts/Level 1 Scripts/Keypad.cs
@@ -37,7 +37,7 @@ public class Keypad : MonoBehaviour
 
     IEnumerator ResetKeypad()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         Ans.text = "";
     }
 
