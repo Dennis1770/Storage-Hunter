6bd139ec043bfa25bbdfe94aac3f8b1cb872add6
LeeAaronVan <lee.aaron.van@gmail.com>
2023-05-08 22:52:40 -0400

    Decorated Storage rooms, fixed dock invisible colliders, and dialogue for level 3

---
+++ Storage Hunter/Assets/Scripts/Level 3 Scripts/CameraDialogue.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDialogue : MonoBehaviour
+{
+
+
+    [SerializeField]
+    pickUpCamera pickUpCameraScript;
+
+    [SerializeField]
+    private GameObject needEvidenceUI;
+
+    [SerializeField]
+    private GameObject findCameraUI;
+
+    [SerializeField]
+    private GameObject foundCameraUI;
+
+    [SerializeField]
+    private GameObject pictureTimeUI;
+
+    [SerializeField]
+    private GameObject cameraInstructionsUI;
+
+    [Header("Pickup Audio Clip")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private void Awake()
+    {
+        StartCoroutine(StartingDialogue());
+
+    }
+
+    IEnumerator StartingDialogue()
+    {
+        yield return new WaitForSeconds(1);
+        needEvidenceUI.SetActive(true);
+        yield return new WaitForSeconds(3);
+        needEvidenceUI.SetActive(false);
+
+        findCameraUI.SetActive(true);
+        yield return new WaitForSeconds(3);
+        findCameraUI.SetActive(false);
+    }
+
+
+
+    public IEnumerator CameraTime()
+    {
+        yield return new WaitForSeconds(1);
+        foundCameraUI.SetActive(true);
+        yield return new WaitForSeconds(3);
+        foundCameraUI.SetActive(false);
+
+        pictureTimeUI.SetActive(true);
+        yield return new WaitForSeconds(3);
+        pictureTimeUI.SetActive(false);
+
+        cameraInstructionsUI.SetActive(true);
+        yield return new WaitForSeconds(3);
+        cameraInstructionsUI.SetActive(false);
+    }
+
+
+}
---
+++ Storage Hunter/Assets/Scripts/Level 3 Scripts/pickUpCamera.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpCamera : MonoBehaviour
+{
+
+    public GameObject playerCamera; //camera game object
+    public Transform player; //player position
+    public float pickUpRange; //how far player is to pick up object
+
+    public CameraDialogue cameraDialogue;
+    public bool holding; //is player holding the camera
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+        
+        if(!holding) //if the player isn't holding the camera, set script false
+        {
+
+            playerCamera.GetComponent<capturePhoto>().enabled = false; //disable script
+
+        }
+
+        if(holding) //if player is holding the camera, set script true
+        {
+
+            playerCamera.GetComponent<capturePhoto>().enabled = true; //enable script
+            playerCamera.GetComponent<MeshRenderer>().enabled = false; //disable mesh renderer
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 distanceToPlayer = player.position - transform.position; //calculate player position
+        //If player is in range of object and presses the E key, call the pickup function
+        if (distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E))
+        {
+
+            PickUp();
+
+        }
+
+    }
+
+    private void PickUp()
+    {
+
+        holding = true; //player is holding the camera
+        playerCamera.GetComponent<capturePhoto>().enabled = true; //enable script
+        playerCamera.GetComponent<MeshRenderer>().enabled = false; //disable mesh renderer
+
+        StartCoroutine(cameraDialogue.CameraTime());
+
+    }
+
+}
