40b674802158f64246a33977c04d2f6c98734a13
michael h <hellomichael99@gmail.com>
2023-05-09 03:53:59 -0400

    Added dale dialogue in park.
    WIP. The script bones are there to allow him to react to the player's progress.

---
+++ Storage Hunter/Assets/Ink/Stories/NPC/Dale_Park.ink
@@ -0,0 +1,59 @@
+->noClue
+
+=== noClue ===
+Betcha came running as soon as you saw the paper I left on your desk. 
+
+This town's in deep shit.
+*[Good to see you too, Dale.] ->one
+*[Mhm.] Alright, don't just stand there. ->one
+*[LEAVE] Wow, nothing to say huh..?->END
+
+==one
+We need to be quick. There're already police here...
+*[Police?] Yeah, cops. Anyways w->two
+*[What are we doing?] W->two
+
+==two
+e're tracking the freak.
+
+Just keep your eyes open, okay? If you see any slime, let me know.
+
+*[Sure thing.] ->three
+*[This is messed up.] ->four
+
+==three
+Oh and one last thing.
+
+The less the cops know, the better. Don't tell them about our investigation.
+
+I don't want them meddling.
+->END
+
+==four
+What?
+*[A girl died..] Look, b-> five
+*[I heard a hiker went missing too.] B-> five
+
+==five
+ad things happen.
+
+That's why I'm here. To make things right.
+
+You're not afraid are you?
+*[I'm not afraid.] Good. Everyone in this town -> six
+*[There's nothing to be afraid of.] ->seven
+*[I am afraid.] Don't overthink this. You and everyone else ->six
+
+==six
+will be safe once the beast is dead. 
+
+And more importantly, Hannah will know I'm not crazy.
+
+I'm counting on you to help me.
+*[Okay.]->three
+
+==seven
+We'll see what you think after our investigation.
+
+Like I said, I don't care if you think I'm crazy.
+->three
--- Storage Hunter/Assets/Scripts/Dialogue/npc/DaleDialogue.cs
+++ Storage Hunter/Assets/Scripts/Dialogue/npc/DaleDialogue.cs
@@ -6,29 +6,52 @@ public class DaleDialogue : MonoBehaviour
 {
     DaleStateManager daleFSM;
 
-    [Header("Ink JSON")][SerializeField] private TextAsset inkJSON;
+    [Header("Ink JSON")][SerializeField] private TextAsset[] inkJSON;
 
     private int i;
+    [SerializeField] private bool hasClue1;
+    [SerializeField] private bool hasClue2;
+    [SerializeField] private bool hasClue3;
+    [SerializeField] private int index;
 
     private void Start()
     {
         i = 0;
+        index = 0;
         daleFSM = FindObjectOfType<DaleStateManager>();
     }
 
     private void Update()
     {
+        ChooseDialogue();
         if (daleFSM.isTalking == true)
         {
             if (i < 1)
             {
                 i++;
-                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON[index]);
             }
         }
         if (daleFSM.isTalking == false)
         {
             i = 0;
+        }
+    }
+
+    private void ChooseDialogue()
+    {
+        if (hasClue1 == true && hasClue2 == true && hasClue3 == true)
+        {
+            index = 2;
+            //tell the player about heading to the dock
+        }
+        else if (hasClue1 == true || hasClue2 == true || hasClue3 == true)
+        {
+            index = 1;
+        }
+        else
+        {
+            index = 0;
         }
     }
 }
